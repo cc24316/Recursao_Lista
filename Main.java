0e37ea80ef5740e7845641173d0c518fde931aed
cc24316 <162496392+cc24316@users.noreply.github.com>
2026-08-21 20:17:47 -0300

    Create Main.java

---
+++ Main.java
@@ -0,0 +1,19 @@
+public class Main
+{
+    public static void main (String[] args)
+    {
+        System.out.println(MatematicaReinventada.isNegativo(-3));
+        System.out.println(MatematicaReinventada.somar(3,2));
+        System.out.println(MatematicaReinventada.subtrair(3,2));
+        System.out.println(MatematicaReinventada.modulo(-3));
+        System.out.println(MatematicaReinventada.multiplicacao(3,2));
+        System.out.println(MatematicaReinventada.potencia(3,2));
+        System.out.println(MatematicaReinventada.divisao(10,2));
+        System.out.println(MatematicaReinventada.resto(10,3));
+        System.out.println(MatematicaReinventada.isPrimeiroMenor(2,3));
+        System.out.println(MatematicaReinventada.isPrimeiroMenorIgual(2,3));
+        System.out.println(MatematicaReinventada.isPrimeiroMaior(3,2));
+        System.out.println(MatematicaReinventada.isPrimeiroMaiorIgual(3,2));
+        System.out.println(MatematicaReinventada.isDiferente(2,3));
+    }
+}
